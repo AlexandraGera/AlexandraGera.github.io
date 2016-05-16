d9468cf054ad53e2bc89c5042777a5090552f648
AlexandraGera <gangrel773@gmail.com>
2016-05-16 20:55:48 +0300

    fixed

--- js/js1_2/js/script.js
+++ js/js1_2/js/script.js
@@ -1,46 +1,38 @@
+//возведение в степень
 function raisedToThePower(number,exponent){
 	var result=number;
-	for (var i=1; i<exponent; i++){
-		result *=number;
+	if (exponent==0){
+		result=1;
+		return result;
+	};
+	if (exponent<0){
+		exponent=exponent*-1;
+		for (var i=1; i<exponent; i++){
+			result *=number;
+		};
+		var negativeExponentResult=1/result;
+		return negativeExponentResult;
+	}else{
+		for (var i=1; i<exponent; i++){
+			result *=number;
+		};
+		return result;
 	}
-	return result;
-}
-var number= prompt('введите число, которое нужно возвести в степень' , '');
-if(number <=0){
-	alert('Число ' + number +
-		'не поддерживается, введите целое число, большее 0'
-		);
-} 
-if(number >0){
-	var exponent= prompt('в какую степень возвести?', 2);
+
 };
-if (exponent < 1) {
-	alert('Степень ' + exponent +
-		'не поддерживается, введите целую степень, большую 0'
-		);
-} else {
-	alert( raisedToThePower(number, exponent) );
-}
+var number= prompt('Введите целое число, которое нужно возвести в степень' , '');
+var exponent= prompt('В какую степень возвести? (любое целое число)', 2);
+alert( raisedToThePower(number, exponent) );
+
 var repeat= confirm("Возвести другое число в степень?");
 if (repeat==true){
-	var number= prompt('введите число, которое нужно возвести в степень' , '');
-	if(number <=0){
-		alert('Число ' + number +
-			'не поддерживается, введите целое число, большее 0'
-			);
-	}
-	if(number >0){
-		var exponent= prompt('в какую степень возвести?', 2);
-	};
-	if (exponent < 1) {
-		alert('Степень ' + exponent +
-			'не поддерживается, введите целую степень, большую 0'
-			);
-	} else {
-		alert( raisedToThePower(number, exponent) );
-	}
-}
+	var number= prompt('Введите целое число, которое нужно возвести в степень' , '');
+	var exponent= prompt('В какую степень возвести? (любое целое число)', 2);
+	alert( raisedToThePower(number, exponent) );
+};
 
+
+//Работа с массивом
 var arrayName = []; 
 
 for(var i=0;i < 5;i++)
