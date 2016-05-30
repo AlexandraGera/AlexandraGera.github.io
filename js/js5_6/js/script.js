2bf090eae4a83ee83e76388305c7f4e2d8761bdc
AlexandraGera <gangrel773@gmail.com>
2016-05-30 14:24:22 +0300

    fixed

--- js/js5_6/js/script.js
+++ js/js5_6/js/script.js
@@ -1,7 +1,6 @@
-var ms='00';
-var s='00';
-var m='00';
-var h='00';
+
+var h, m, s, ms;
+ms=s=m=h='00';
 var counter=0;
 var counterSplit=0;
 var timer;
@@ -13,51 +12,53 @@ var boxSplit = document.querySelector('div.box-split');
 var timerShow=document.querySelector('p.timer');
 
 timerShow.innerHTML=  h +':'+ m +':'+ s +':'+ ms;
+function addZero(time){
+	if(time < 10){
+		time='0' + time;
+	};
+	return time;
+};
+function HGo() {
+	h++;
+	h=addZero(h);
+	if(h > 23){
+		ms=s=m=h='00';
+	}
+};
+function MGo() {
+	m++;
+	m=addZero(m);
+	if(m > 59){
+		m='00';
+		HGo();
+	}
+};
+function SGo() {
+	s++;
+	s=addZero(s);
+	if(s > 59){
+		s='00';
+		MGo();
+	}
+};
+function MsGo() {
+	ms++;
+	ms=addZero(ms);	
+	if(ms>99){
+		ms='00';
+		SGo();
+	};
+};
 
 function timeGo() {  
-
-	ms++;
-
-	if (ms < 10){ 
-		ms = '0' + ms;
-	};
-	if (ms > 99) {
-		ms = '00';
-		s++;
-
-		if (s < 10){ 
-			s = '0' + s; 
-		};
-		if (s > 59 ) {
-			s = '00';
-			m++;
-
-			if (m < 10) {
-				m = '0' + m;
-			};
-			if (m > 59) {
-				m = '00';
-				h++;
-
-				if (h < 10) {
-					h = '0' + h;
-				};
-				if (h > 23) {
-					h = '00';
-					m = '00';
-					s = '00';
-					ms = '00';
-				};
-			};
-		};
-	};  
+	MsGo();
 	timerShow.innerHTML = h + ':' + m + ':' + s + ':' + ms;
 	timer = setTimeout( timeGo , 10);
 };
 
 function startStopButton() {
 	counter++;
-console.log(counter);
+	
 	if (counter % 2 !== 0) {
 		start.innerHTML = 'Stop';  
 		timeGo();
@@ -81,16 +82,11 @@ function resetButton() {
 
 	clearTimeout(timer);
 
-	ms = '00';
-	s = '00';
-	m = '00';
-	h = '00';
-
+	ms=s=m=h='00';
 	timerShow.innerHTML=  h +':'+ m +':'+ s +':'+ ms;
 	boxSplit.innerHTML= " ";
 
-	counterSplit = 0;
-	counter = 0;
+	counterSplit=counter=0;
 };
 
 start.addEventListener('click', startStopButton);
